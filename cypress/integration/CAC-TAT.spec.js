914933d042a0e86a15dc3a5bc8f74246dba7b2f4
Claudio Eloi <junior040712@gmail.com>
2022-12-05 13:38:34 -0300

    Erro

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -9,7 +9,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
     })
 
     it('verifica o título da aplicação', function() {        
-        cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+        cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT00')
     })
 
     it('preenche os campos obrigatórios e envia o formulário', function(){
